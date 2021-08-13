f2f009e9bb9ffa81b602cec0f93ca9b9d5d56415
Rodrigo Villalobos <xravg.rodrigo@gmail.com>
2021-08-13 12:08:23 -0600

    add functionality, still there are some bugs

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,5 @@
+// THEME CHANGER 
+
 let icon = document.getElementById('theme-selector');
 icon.value = 1;
 document.body.className = 'theme-1'
@@ -13,3 +15,89 @@ const themeFunction = () => {
 }
 
 icon.addEventListener('change', themeFunction);
+
+///////////////////////////////////////////////////////
+
+let operatorA = "";
+let operatorB = "";
+let memory = 0;
+
+
+let buttons = document.querySelector('.number-grid');
+let display = document.getElementById('result');
+let calculator = document.querySelector('.wrapper');
+
+display.value = "";
+
+const resetFunction = () => {
+    display.value = "";
+    operatorA = "";
+    operatorB = "";
+    memory = 0;
+}
+
+const numberUpdater = (value) => {
+    operatorA += value;
+    display.value = operatorA;
+}
+
+const multiplyFunction = () => {
+    operatorB = operatorA;
+    operatorA = "";
+    memory = 5;
+}
+
+const addFunction = () => {
+    operatorB = operatorA;
+    operatorA = "";
+    memory = 4;
+}
+
+const substractFunction = () => {
+    operatorB = operatorA;
+    operatorA = "";
+    memory = 3;
+}
+
+const calculateFunction = () => {
+    if (memory === 5) {
+        display.value = parseFloat(operatorA) * parseFloat(operatorB);
+        memory = 0;
+    } else if (memory === 4) {
+        display.value = parseFloat(operatorA) + parseFloat(operatorB);
+        memory = 0;
+    } else if (memory === 3) {
+        display.value = parseFloat(operatorB) - parseFloat(operatorA);
+        memory = 0;
+    }
+}
+
+/////////////////////////////////
+const mainFunction = (e) => {
+    let actionElement = e.target.dataset.action;
+
+    if (!actionElement){
+        numberUpdater(e.target.textContent);
+    } else if (actionElement === "decimal") {
+        operatorA += ".";
+    } else if (actionElement === "clear") {
+        resetFunction();
+    } else if (actionElement === "multiply") {
+        multiplyFunction();
+    } else if (actionElement === "add") {
+        addFunction();
+    } else if (actionElement === "calculate") {
+        calculateFunction();
+    } else if (actionElement === "subsctraction") {
+        substractFunction();
+    } else if (actionElement === "delete") {
+        operatorA = display.value.slice(0, -1);
+        display.value = operatorA;
+    }
+        
+    
+};
+
+
+buttons.addEventListener('click', mainFunction);
+
